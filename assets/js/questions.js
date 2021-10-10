765c2840bdb377cd7cf945429dbee6c210ea231b
Caroline Pastorelli <caroline.pastorelli@gmail.com>
2021-10-09 23:46:17 -0700

    changed JSON to another JS file. Added comments and more styling to CSS file. changed how options appeared on HTML file

---
+++ assets/js/questions.js
@@ -0,0 +1,87 @@
+var questions = [
+    
+    {
+        number: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        answerChoices:[
+            "How To Make Lentils",
+            "Hotpocket Tamale Mango Lasagna",
+            "Hyper Text Markup Language",
+            "Horrific Theater Makeup Lessons"        
+        ] ,
+        
+        
+    },
+
+   
+    {
+        numb: 2,
+        question: "CSS means what exactly?",
+        answer: "Hyper Text Markup Language",
+        answerChoices:[
+            "Cascading Style Sheets",
+            "Canadian Salted Squid",
+            "Californian Society of Surfers",
+            "Chili Sourcream Soup"
+        ],
+
+           
+    },
+
+    {
+        numb: 3,
+        question: "What is the purpose of CSS?",
+        answer: "Hyper Text Markup Language",
+        answerChoices:[
+            "To create functionality to a website",
+            "Add styling to a website",
+            "Add database connection to a website",
+            "To make life more complicated"
+        ],
+
+       
+    },
+
+    {
+        numb: 4,
+        question: "What is the purpose of JavaScript?",
+        answer: "Hyper Text Markup Language",
+        answerChoices:[
+            "To add functionality to the website",
+            "To create databases",
+            "To make web pages semantic",
+            "To make web pages interactive"
+        ],
+
+         
+    },
+
+    {
+        numb: 5,
+        question:"What does DOM stand for in HTML?",
+        answer: "Hyper Text Markup Language",
+        answerChoices:[
+            "Defense of Marsupials",
+            "Department of Mysteries",
+            "Document Object Model",
+            "Doctor of Oriental Medicine"
+        ],
+         
+    },
+
+    {
+        numb: 6,
+        question: "Why is DOM important?",
+        answer: "Hyper Text Markup Language",
+        answerChoices:[
+            "It acts as a programming interface for web documents",
+            "It will create a website without any need for programming",
+            "It will make a document of model trains",
+            "If used incorrectly, the DOM will recreate Skynet and take over the world"
+        ],
+
+        
+    }
+]   
+   
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,176 @@
-var counter = document.querySelector();
-var answer = document.querySelector()
-var wrongAnswer = document.querySelector();
-var correctAnswer = document.querySelector();
-var beginQuiz = document.querySelector(#beginQuiz);
+
+var questionPrompt = document.querySelector(".showQ");
+var answerPrompt = document.querySelector(".showA");
+var showGameOver = document.querySelector(".gameEnd");
+var timerEl= document.querySelector(".timer-count");
+var loss = document.querySelector(".lose");
+var win = document.querySelector(".win");
+
+
 var quizScore = 0;
-var didTimeRunOut;
-var didGameStart = false;
-var isGameOver = false;
+var timerCount = 30;
+var winCount = 0;
+var lossCount = 0;
+var isWinner = false;
+var timerCount;
+var timer;
+
 
 var userInitial = localStorage.getItem("userInitial");
 
 var questions;
 
-beginQuiz.addEventListener("click", commenceQuiz);
 
+function startQuiz(){
+    isWinner = false;
+    
+    timerCount = 10;
+    
+    document.addEventListener("DOMContentLoaded", function(event) {
+        document.getElementById("startQuizButton").disabled = true;
+      });
+
+    startTimer();
+    //showQuestions(0);
+}
+    //localstorage set item
+        // var previousQuizInfo = {
+    //     username: userInitial.value.trim(),
+    //     finalScore: quizScore.value,
+    //     timedOut: didTimeRunOut.value,
+    // }
+
+
+//localStorage.setItem("user", JSON.stringify(quizObject));
+// OR
+    // var quizObject = JSON.stringify(previousQuizInfo);
+    // localStorage.setItem("user", quizObject);
+
+
+// function showQuestions(i) {
+    
+//     let questionTag =  questions[i].number + ". " + questions[i].question ;
+    
+//     let optionsTag = '<option value=1>'+ questions[i].answerChoices[0] +'</option>'
+//     + '<option value=2>'+ questions[i].answerChoices[1] +'</option>'
+//     + '<option value=3>'+ questions[i].answerChoices[2] +'</option>'
+//     + '<option value=4>'+ questions[i].answerChoices[3] +'</option>';
+
+//     questionPrompt.innerHTML = questionTag;
+//     answerPrompt.innerHTML = optionsTag;
+
+//     //const optionsToMake = answerPrompt.querySelectAll(".showA");
+
+//     //const options = answerPrompt.querySelectAll(".answerChoices");
+
+//     for (index = 0; index < 4; index++) {
+
+//         answerChoices[index].setAttribute("onclick", "selectedAnswer(this)");
+//     }
+
+// }
+
+function startTimer() {
+   console.log("Start Timer has begun.");
+    timer = setInterval( function() {
+        
+        timerCount--;
+        console.log("TimerCount decreased by a second");
+        
+        timerEl.textContent = timerCount;
+
+        if(timerCount>= 0) {
+
+            if(isWinner && timerCount > 0)
+            {
+                clearInterval(timer);
+                console.log("I am in If statement is winner && timercount > 0");
+                gameWin();
+            }
+        }
+         
+        if(timerCount === 0) {
+            console.log("timer is now zero");
+            clearInterval(timer);
+            gameLose();
+        }
+
+    }, 1000);
+}
+
+// function selectedAnswer(answer) {
+
+//  let userAnswer = answer.textContent; //get user selected option
+
+//  let correctAnswer = questions[index].answer;
+
+//  const allOptions = answerChoices.children.length;
+
+//  if (userAnswer == correctAnswer) {
+
+//     quizScore += 1;
+
+//     answer.classList.add("correct");
+
+//     answer.insertAdjacentHTML("beforeend", tickIconTag);
+
+//     console.log("Correct Answer!");
+
+//     }else {
+//         answer.classList.add("incorrect");
+
+//         answer.insertAdjacentHTML.apply("beforeend", crossIconTag);
+
+//         console.log("Wrong Answer");
+//     }
+
+//     for (i = 0; i < allOptions; i++){
+
+//         console.log("I don't know what to do really");
+
+//         //answerChoices..children[i].classList.add("disabled");
+//     }
+
+// }
+
+function gameWin(){
+
+    console.log("you won!")
+
+    document.addEventListener("DOMContentLoaded", function(event) {
+
+        document.getElementById("startQuizButton").disabled = false;
+      });
+
+    winCount++;
+
+    setWin();
+}
+
+function gameLose(){
+
+    console.log("you lose!")
+
+    document.addEventListener("DOMContentLoaded", function(event) {
+
+        document.getElementById("startQuizButton").disabled = false;
+      });
+
+    lossCount++;
+
+    setLoss();
+}
+
+function setWin() {
+
+    win.textContent = winCount;
+
+    localStorage.setItem("winCount", winCounter);
+}
+
+function setLoss() {
+
+    loss.textContent = lossCount;
+
+    localStorage.setItem("lossCount", lossCount);
+}
